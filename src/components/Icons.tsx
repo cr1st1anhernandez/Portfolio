4de695b6ad56c9170963f9ec3ed17d9c83300977
Cristian Hernandez Ramirez <cristian0402218@gmail.com>
2024-07-27 16:39:29 -0600

    💄 Update Projects style

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -28,6 +28,26 @@ export const InstagramIcon = (props: React.SVGProps<SVGSVGElement>) => (
 	</svg>
 );
 
+export const ArrowUpIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={24}
+		height={24}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path d="M16.5 7.5L6 18" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+		<path
+			d="M8 6.18791C8 6.18791 16.0479 5.50949 17.2692 6.73079C18.4906 7.95209 17.812 16 17.812 16"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
 export const Linkedin01Icon = (props: React.SVGProps<SVGSVGElement>) => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,51 +1,48 @@
 import LinuxLearner from '../assets/linuxLearner.png';
 import SpotifySearcher from '../assets/spotifySearcher.png';
-import { PinContainer } from './3dPin';
-import { GitMergeIcon, NextJsIcon, ReactIcon, TailwindIcon } from './Icons';
+import { ArrowUpIcon, GitMergeIcon, NextJsIcon, ReactIcon, TailwindIcon } from './Icons';
 
 export const Projects = () => {
 	return (
 		<div className="text-lg bg-[#1D1C20] flex flex-col gap-8 rounded-lg p-6 pb-12 lg:p-12 col-span-3 font-normal text-neutral-400 lg:pb-24">
 			<h2 className="text-2xl lg:text-4xl pb-4 font-bold text-neutral-100">Projects</h2>
-			<section className="flex w-full flex-col md:flex-row gap-20 md:gap-8">
-				<PinContainer title="linux-learner.vercel.app/" href="https://linux-learner.vercel.app/">
-					<div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[14rem] lg:h-[20rem] lg:w-[20rem]">
-						<h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-							Linux Learner
-						</h3>
-						<span className="text-neutral-500 ">A website to learn linux for beginners.</span>
-						<div className="rounded-lg overflow-hidden mt-4">
-							<img src={LinuxLearner} alt="Linux Learner" className="w-full h-full" />
-						</div>
-						<footer className="flex gap-2">
-							<NextJsIcon width={20} />
-							<TailwindIcon width={20} />
-							<ReactIcon width={20} />
-							<GitMergeIcon width={20} />
-						</footer>
-					</div>
-				</PinContainer>
-				<PinContainer
-					title="spotify-searcher-artist.vercel.app/"
-					href="https://spotify-searcher-artist.vercel.app/">
-					<div
-						className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2
-           w-[14rem] lg:h-[20rem] lg:w-[20rem]">
-						<h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
-							Spotify Searcher
-						</h3>
-						<span className="text-neutral-500 ">A website for search artist in spotify.</span>
-						<div className="rounded-lg overflow-hidden mt-4">
-							<img src={SpotifySearcher} alt="Linux Learner" className="w-full h-full" />
-						</div>
-						<footer className="flex gap-2">
-							<NextJsIcon width={20} />
-							<TailwindIcon width={20} />
-							<ReactIcon width={20} />
-							<GitMergeIcon width={20} />
-						</footer>
-					</div>
-				</PinContainer>
+			<section className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:grid-cols-3">
+				<a
+					href="https://linux-learner.vercel.app/"
+					target="_blank"
+					rel="noreferrer"
+					className="block border-2 rounded-lg border-[#414044] shadow-md shadow-black cursor-pointer ease-in-out duration-300 transition-transform transform hover:scale-105 p-4">
+					<header className="flex justify-between items-center">
+						<h3 className="text-xl font-bold text-neutral-100">Linux Learner</h3>
+						<ArrowUpIcon width={20} />
+					</header>
+					<p className="text-neutral-500">A website to learn linux for beginners.</p>
+					<img src={LinuxLearner} alt="Linux Learner" className="w-full rounded-lg mt-4" />
+					<footer className="flex gap-2">
+						<NextJsIcon width={20} />
+						<TailwindIcon width={20} />
+						<ReactIcon width={20} />
+						<GitMergeIcon width={20} />
+					</footer>
+				</a>
+				<a
+					href="https://spotify-searcher-artist.vercel.app/"
+					target="_blank"
+					rel="noreferrer"
+					className="block border-2 rounded-lg border-[#414044] shadow-md shadow-black cursor-pointer ease-in-out duration-300 transition-transform transform hover:scale-105 p-4">
+					<header className="flex justify-between items-center">
+						<h3 className="text-xl font-bold text-neutral-100">Spotify Searcher</h3>
+						<ArrowUpIcon width={20} />
+					</header>
+					<p className="text-neutral-500">A website for search artists in spotify.</p>
+					<img src={SpotifySearcher} alt="SpotifySearcher" className="w-full rounded-lg mt-4" />
+					<footer className="flex gap-2">
+						<NextJsIcon width={20} />
+						<TailwindIcon width={20} />
+						<ReactIcon width={20} />
+						<GitMergeIcon width={20} />
+					</footer>
+				</a>
 			</section>
 		</div>
 	);
